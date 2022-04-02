29b2893efe1a1a0f4d099bdb7ac8cc01fd8fdeb1
Viktoriia <shevictory@gmail.com>
2022-04-02 19:04:23 +0300

    Add SelectedUser List Child=>Child

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { Component } from 'react';
+import SelectedUsersList from './components/SelectedUsersList';
 import UsersList from './components/UsersList';
 
-function App() {
-  return <UsersList />;
+// IMPORTANT!!!
+// Повторяющиеся элементы списка должны иметь проп key,
+// "приколоченный" к передаваемым пропсам
+
+// Взаимодействие компонентов:
+// 1 Parent => Child: props
+// 2 Child => Parent: cb
+// 3 Child => Child: use parent
+
+const usersDB = [
+  { id: 1, firstName: 'Test', lastName: 'Testovich' },
+  { id: 2, firstName: 'John', lastName: 'Dou' },
+  { id: 3, firstName: 'Jane', lastName: 'Dou' },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: usersDB.map(u => ({ ...u, isSelected: false })),
+    };
+  }
+
+  setUsers = newUsers => {
+    this.setState({ users: newUsers });
+  };
+
+  render() {
+    const { users } = this.state;
+    return (
+      <>
+        <SelectedUsersList users={users} />
+        <UsersList users={users} setUsers={this.setUsers} />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/UsersList/index.js
+++ src/components/UsersList/index.js
@@ -1,57 +1,20 @@
 import React, { Component } from 'react';
 import UsersListItem from './UsersListItem';
 
-// IMPORTANT!!!
-// Повторяющиеся элементы списка должны иметь проп key,
-// "приколоченный" к передаваемым пропсам
-
-// Взаимодействие компонентов:
-// 1 Parent => Child: props
-// 2 Child => Parent: cb
-
-const usersDB = [
-  { id: 1, firstName: 'Test', lastName: 'Testovich' },
-  { id: 2, firstName: 'John', lastName: 'Dou' },
-  { id: 3, firstName: 'Jane', lastName: 'Dou' },
-];
-
-class UsersList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      users: usersDB.map(u => ({ ...u, isSelected: false })),
-    };
-  }
-  mapUser = (u, index) => {
+function UsersList({ users, setUsers }) {
+  const mapUser = (u, index) => {
     const selectUser = () => {
-      const { users } = this.state;
       const newUsers = [...users];
 
       newUsers[index].isSelected = !newUsers[index].isSelected;
 
-      this.setState({ users: newUsers });
+      setUsers(newUsers);
     };
 
     return <UsersListItem key={u.id} user={u} selectUser={selectUser} />;
   };
 
-  // selectUser = id => {
-  //   const { users } = this.state;
-  //   const newUsers = [...users];
-
-  //   const selectedUserIndex = newUsers.findIndex(u => u.id === id);
-
-  //   newUsers[selectedUserIndex].isSelected =
-  //     !newUsers[selectedUserIndex].isSelected;
-
-  //   this.setState({ users: newUsers });
-  // };
-
-  render() {
-    const { users } = this.state;
-    return <ul>{users.map(this.mapUser)}</ul>;
-  }
+  return <ul>{users.map(mapUser)}</ul>;
 }
 
 export default UsersList;
